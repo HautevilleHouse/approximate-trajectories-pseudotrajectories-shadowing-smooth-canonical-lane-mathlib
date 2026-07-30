8461c35fedd592eaa1d9ae7cdccc9319cf0f18e7
Hauteville House <pgjkmqpw52@privaterelay.appleid.com>
2026-07-29 20:38:36 -0400

    Canon

---
+++ ApproximateTrajectoriesPseudotrajectoriesShadowingSmoothCanonicalLaneLean/FinalTheorem.lean
@@ -0,0 +1,12 @@
+import ApproximateTrajectoriesPseudotrajectoriesShadowingSmoothCanonicalLaneLean.GateLemmas
+
+namespace HautevilleHouse
+namespace ApproximateTrajectoriesPseudotrajectoriesShadowingSmoothCanonicalLaneLean
+
+def ConstrainedTheoremClosure (A : AdmissibleClass) : Prop := bridgeClosed A ∧ gateClosed A
+
+theorem constrained_theorem_closure (A : AdmissibleClass) : ConstrainedTheoremClosure A := by
+  exact And.intro (bridge_from_admissible_class A) (gate_from_admissible_class A)
+
+end ApproximateTrajectoriesPseudotrajectoriesShadowingSmoothCanonicalLaneLean
+end HautevilleHouse
